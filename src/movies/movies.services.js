a67014660b5a694e9b3a15b22d9f0fb2b14ee8f3
msi43089 <msi43089@gmail.com>
2021-10-05 22:21:19 -0400

    Finished Reviews

--- src/movies/movies.services.js
+++ src/movies/movies.services.js
@@ -1,3 +1,4 @@
+const { text } = require("express");
 const { orderBy, queryBuilder } = require("../db/connection");
 const knex = require("../db/connection")
 const mapProperties = require("../utils/map-properties")
@@ -41,14 +42,23 @@ function movieTheater(movieId) {
         .where({"mt.is_showing": true})
 }
 
+const addCritic = mapProperties({
+  critic_id: "critic.critic_id",
+  preferred_name: "critic.preferred_name",
+  surname: "critic.surname",
+  organization_name: "critic.organization_name",
+})
 
-//need to update to add nested critics object
+
 function movieReview(movieId){
     return knex("movies as m")
         .join("reviews as r", "r.movie_id", "m.movie_id")
         .join("critics as c", "c.critic_id", "r.critic_id")
-        .select("r.*")
+        .select("*")
         .where({"m.movie_id": movieId})
+        .then((reviews) => {
+            return reviews.map((review) => addCritic(review))
+            })
 }
 
 
